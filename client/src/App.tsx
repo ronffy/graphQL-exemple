4b1b5fa4cfff0f2458aed6635746321f9870e8ed
ronffy <ronffy@sina.com>
2019-09-27 15:21:53 +0800

    first add

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+// import './theme.css';
+// import './App.css';
+// import logo from './spacex-logo-light.png'
+import Launches from './Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          {/* <img src={logo} id="logo" /> */}
+          <Launches />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Launches.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
+
+const LAUNCHES_QUERY = gql`
+  query QUERY_LAUNCH {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+    launch(flight_number: 3) {
+      flight_number
+      mission_name
+      launch_success
+      launch_date_local
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
+
+console.log('LAUNCHES_QUERY', LAUNCHES_QUERY);
+
+const Launches = (props) => {
+
+  return (
+    <Fragment>
+      <h1 className="display-4 my-3">Launches</h1>
+
+      <Query query={LAUNCHES_QUERY}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>
+            if (error) console.log(error);
+            return (
+              <>
+                <LaunchItem key={data.launch.flight_number} launch={data.launch} />
+                <hr />
+                <Fragment>
+                  {
+                    data.launches.map((launch) =>
+                      <LaunchItem key={launch.flight_number} launch={launch} />
+                    )
+                  }
+                </Fragment>
+              </>
+            )
+          }
+        }
+      </Query>
+    </Fragment>
+  )
+}
+
+export default Launches
